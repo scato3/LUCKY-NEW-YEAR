8d4321dbedd400f9da6fbe77eccde47e4c3c5d64
scato3 <tlsgustn93@naver.com>
2025-01-27 12:21:48 +0000

    fix: ux 수정

--- src/components/common/OptimizedImage.tsx
+++ src/components/common/OptimizedImage.tsx
@@ -1,9 +1,14 @@
 import Image, { ImageProps } from 'next/image';
 
-export const OptimizedImage = ({ className, ...props }: ImageProps) => (
+export const OptimizedImage = ({
+  className,
+  alt = '',
+  ...props
+}: ImageProps) => (
   <Image
     {...props}
     className={className}
+    alt={alt}
     quality={75}
     priority
     loading="eager"
