35b47bc6462df915100ce341062fe49070476d5a
scato3 <tlsgustn93@naver.com>
2025-01-27 13:16:48 +0000

    fix: searchparams를 client에서 처리

--- src/app/v/[uuid]/ranking/detail/page.tsx
+++ src/app/v/[uuid]/ranking/detail/page.tsx
@@ -3,18 +3,14 @@ import { Suspense } from 'react';
 
 interface PageProps {
   params: Promise<{ uuid: string }>;
-  searchParams: Promise<{
-    uuid: string;
-  }>;
 }
 
-export default async function DetailPage({ params, searchParams }: PageProps) {
+export default async function DetailPage({ params }: PageProps) {
   const { uuid } = await params;
-  const { uuid: searchUuid } = await searchParams;
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
-      <DetailClient ownerUUID={uuid} findUUID={searchUuid} />
+      <DetailClient ownerUUID={uuid} />
     </Suspense>
   );
 }
