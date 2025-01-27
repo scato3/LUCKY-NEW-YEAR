e09e5acb79f144b64ede8a7b898ce49e7a3acab2
scato3 <tlsgustn93@naver.com>
2025-01-27 07:44:47 +0000

    fix:params 변경

--- src/app/v/[uuid]/boiling/page.tsx
+++ src/app/v/[uuid]/boiling/page.tsx
@@ -1,11 +1,5 @@
 import BoilingTteokguk from '@/components/pages/boiling-tteokguk/boiling-tteokguk';
 
-interface Props {
-  params: {
-    uuid: string;
-  };
-}
-
-export default function BoilingPage({ params }: Props) {
+export default async function Page({ params }: { params: { uuid: string } }) {
   return <BoilingTteokguk uuid={params.uuid} />;
 }
