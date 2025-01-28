f3f2cfcdd4ed10ebc39a76a35745a5466e3659ae
scato3 <tlsgustn93@naver.com>
2025-01-28 14:15:21 +0000

    fix: svg to png

--- public/tteokguk/index.ts
+++ public/tteokguk/index.ts
@@ -1,8 +1,8 @@
-export { default as NormalTteokguk } from './normal_tteokguk.svg';
-export { default as FullTteokguk } from './full_tteokguk.svg';
-export { default as DakTteokguk } from './dak_tteokguk.svg';
-export { default as HaemulTteokguk } from './haemul_tteokguk.svg';
-export { default as MaraTteokguk } from './mara_tteokguk.svg';
-export { default as MyulchiTteokguk } from './myulchi_tteokguk.svg';
-export { default as SagolTteokguk } from './sagol_tteokguk.svg';
-export { default as TomatoTteokguk } from './tomato_tteokguk.svg';
+export { default as NormalTteokguk } from './normal_tteokguk.png';
+export { default as FullTteokguk } from './full_tteokguk.png';
+export { default as DakTteokguk } from './dak_tteokguk.png';
+export { default as HaemulTteokguk } from './haemul_tteokguk.png';
+export { default as MaraTteokguk } from './mara_tteokguk.png';
+export { default as MyulchiTteokguk } from './myulchi_tteokguk.png';
+export { default as SagolTteokguk } from './sagol_tteokguk.png';
+export { default as TomatoTteokguk } from './tomato_tteokguk.png';
